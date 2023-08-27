9a1dd31e29f1655ceaa0c73f2b9593373529f9ad
Ratul <rajaulislamratul12@gmail.com>
2023-08-27 19:00:06 +0600

    commit change

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,15 @@ const ratingNum = document.querySelector(".rating-num");
 let rating;
 for (let i = 0; i < span.length; i++) {
   span[i].addEventListener("click", () => {
-    rating = span[i].textContent;
-    span[i].style.backgroundColor = "hsl(25, 97%, 53%)";
-    span[i].style.Color = "white";
-    span[i].style.borderColor = "hsl(25, 97%, 53%)";
+    if (rating) {
+      rating.style.color = "#7C8798";
+      rating.style.backgroundColor = "#3F4F5E";
+      rating.style.borderColor = "#3F4F5E";
+    }
+    rating = span[i];
+    rating.style.color = "#fff";
+    rating.style.backgroundColor = "hsl(25, 97%, 53%)";
+    rating.style.borderColor = "hsl(25, 97%, 53%)";
   });
 }
 
@@ -16,6 +21,6 @@ submit.addEventListener("click", () => {
   if (rating) {
     document.querySelector("main").classList.add("hidden");
     document.querySelector("article").classList.remove("hidden");
-    ratingNum.textContent = rating;
+    ratingNum.textContent = rating.textContent;
   }
 });
